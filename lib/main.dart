fcd975dacebe9311bdb89c40e0e37c3ba4054067
Alief Alhadi <alief.alhadi@majoo.id>
2024-01-10 11:45:44 +0700

    update version

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Incremen2t',
+        tooltip: 'Incremen234t',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
